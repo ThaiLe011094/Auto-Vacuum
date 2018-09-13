8540a84edb0e86c026cd0d3d15254cec684706e2
Thái Lê <charliebellow.lt@gmail.com>
2018-09-13 13:30:05 +0700

    Create Time.h

---
+++ Time.h
@@ -0,0 +1,12 @@
+#ifndef TIME_H_
+#define TIME_H_
+
+void initTime(void);
+void delay(uint32_t ms);
+void delayMicroseconds(uint32_t us);
+uint32_t millis(void);
+uint32_t micros(void);
+
+
+
+#endif /* TIME_H_ */
