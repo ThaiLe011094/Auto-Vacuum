e8786c4242197d686c2478777ccb59dab42bce20
Thái Lê <charliebellow.lt@gmail.com>
2018-09-13 13:26:51 +0700

    Create PWM_Config.h

---
+++ PWM_Config.h
@@ -0,0 +1,10 @@
+#ifndef PWM_CONFIG_H_
+#define PWM_CONFIG_H_
+
+extern void Config_PWM(void);
+
+extern void Go_Straight(void);
+extern void Turn_Right(void);
+extern void Turn_Left(void);
+extern void Stop(void);
+#endif /* PWM_CONFIG_H_ */
